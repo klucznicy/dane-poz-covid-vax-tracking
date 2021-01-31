cabeb7682370b319f8f5a8adc0733c09542762db
Lucas Garczewski <lgarczewski@gmail.com>
2021-01-31 13:57:44 +0100

    initial commit

---
+++ charts.js
@@ -0,0 +1,130 @@
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+// Load the Visualization API and the corechart package.
+google.charts.load('current', {'packages':['corechart', 'table', 'bar'], 'language': 'pl'});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawChartProgressBar);
+google.charts.setOnLoadCallback(drawChartWeeklyVaxers);
+google.charts.setOnLoadCallback(drawChartTotalVaxers);
+
+docUrl = 'https://docs.google.com/spreadsheets/d/1RQAg-gqb8MfzXF30QC9CCAVpWF5U5YkL8gnnq54q4C8';
+
+function drawChartProgressBar() {
+  var queryString = encodeURIComponent('SELECT A, B, C, D');
+
+  var query = new google.visualization.Query( docUrl + '/gviz/tq?gid=1577334276&tq=' + queryString );
+  query.send(handleQueryResponseProgressBar);
+}
+
+function drawChartWeeklyVaxers() {
+  var queryString = encodeURIComponent('SELECT B, C');
+
+  var query = new google.visualization.Query( docUrl + '/gviz/tq?tq=' + queryString );
+  query.send(handleQueryResponseWeekly);
+}
+
+function drawChartTotalVaxers() {
+  var queryString = encodeURIComponent('SELECT B, D');
+
+  var query = new google.visualization.Query( docUrl + '/gviz/tq?tq=' + queryString );
+  query.send(handleQueryResponseTotal);
+}
+
+function handleQueryResponseProgressBar(response) {
+  var data = response.getDataTable();
+
+  var options = {
+    isStacked: 'percent',
+    animation: {
+      duration: 750,
+      "startup": true
+    },
+    hAxis: {
+      position: 'none'
+    },
+    vAxis: {
+      gridlines: {
+        color: 'transparent'
+      }
+    },
+   legend: {
+      position: 'bottom'
+   },
+   colors: ['#34a853', '#4285f4', '#DC3912'],
+    annotations: {
+      alwaysOutside: true
+    }
+  };
+
+  var view = new google.visualization.DataView(data);
+
+  var chart = new google.visualization.BarChart(document.getElementById('progress_bar'));
+
+  chart.draw(view, options);
+}
+function handleQueryResponseWeekly(response) {
+  var data = response.getDataTable();
+
+  var options = {
+    chart: {title:'Podane pierwsze dawki'},
+    animation: {
+      startup: true,
+      duration: 750,
+    },
+    hAxis: {
+      gridlines: {
+        color: 'transparent'
+      }
+    },
+    legend: { position: 'top' }
+  };
+
+  var chart = new google.visualization.ColumnChart(document.getElementById('weekly_vaxers'));
+
+  chart.draw(data, options);
+}
+
+function handleQueryResponseTotal(response) {
+  var data = response.getDataTable();
+
+  var options = {
+    chart: {title:'Zaszczepieni'},
+    animation: {
+      startup: true,
+      duration: 750,
+    },
+    hAxis: {
+      gridlines: {
+        color: 'transparent'
+      }
+    },
+    legend: {position: 'top'},
+  };
+
+
+  var chart = new google.visualization.AreaChart(document.getElementById('total_vaxers'));
+
+  chart.draw(data, options);
+}
+
+function fetchCSV( type ) {
+  var queryString = encodeURIComponent('SELECT A, B');
+
+  var query = new google.visualization.Query( docUrl + '/gviz/tq?tq=' + queryString );
+  query.send(deliverCSV);
+}
+
+function deliverCSV( response ) {
+  var data = response.getDataTable();
+
+  var csv = google.visualization.dataTableToCsv(data);
+  console.log(csv);
+
+  var hiddenElement = document.createElement('a');
+  hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+  hiddenElement.target = '_blank';
+  hiddenElement.download = 'dane-szczepionki.csv';
+  hiddenElement.click();
+}
+</script>
---
+++ spreadsheet-field.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Spreadsheet field
+ *
+ * @package     spreadhseet-field
+ * @author      Łukasz Garczewski
+ * @copyright   2020 Łukasz Garczewski
+ * @license     GPL-3.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name: Spreadsheet field
+ * Plugin URI:
+ * Description:
+ * Version: 0.1
+ * Author: Łukasz Garczewski
+ * Author URI:
+ * Text Domain: spreadhseet-field
+ * Domain Path: /languages
+ * License: GPL v3 or later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+add_filter( 'the_content', 'lu_substitute_tag', 1 );
+function lu_substitute_tag( string $content ) {
+  $cache_key = 'klucznicy_spreadsheet_field';
+  $fields = array(
+    'spreadsheet_field',
+    'spreadsheet_last_data',
+    'spreadsheet_total_vaxers'
+  );
+
+  $result = wp_cache_get( $cache_key );
+
+  if ( empty( $result ) ) {
+    $url = 'https://docs.google.com/spreadsheets/d/1RQAg-gqb8MfzXF30QC9CCAVpWF5U5YkL8gnnq54q4C8/htmlembed?single=true&gid=1720779961&range=B2:B4&widget=false&chrome=false&headers=false';
+    $response = wp_remote_get( $url );
+
+    if ( !is_array( $response ) || is_wp_error( $response ) ) {
+      return $content;
+    }
+
+    $body    = $response['body']; // use the content
+
+    preg_match_all( '/<td class=\"s[0-9]+\"[ ="a-z]*>([0-9a-z ]+)<\/td>/', $body, $result );
+
+    wp_cache_set( $cache_key, $result, '' /* group */, 60 * 60 );
+  }
+
+  //@TODO: convert this to a shortcode
+  foreach ( $fields as $i => $needle ) {
+    $content = str_replace( $needle, $result[1][$i], $content );
+  }
+
+  return $content;
+}
